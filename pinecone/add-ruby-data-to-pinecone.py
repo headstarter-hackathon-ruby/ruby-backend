bc777d839701d4a021deb2e28779d7da9ecc8261
colexeco <evan.shoemaker0@gmail.com>
2024-08-17 10:09:15 -0700

    instantiate openai

--- pinecone/add-ruby-data-to-pinecone.py
+++ pinecone/add-ruby-data-to-pinecone.py
@@ -1,12 +1,18 @@
+#https://docs.pinecone.io/integrations/openai
 from pinecone import Pinecone, ServerlessSpec
+from openai import OpenAI
 import json
 from pathlib import Path
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 file_path='./ruby_hackathon_data.json'
+index_name = "complaints"
+
 PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 data = json.loads(Path(file_path).read_text())
 
@@ -14,9 +20,9 @@ pc = Pinecone(
     api_key=PINECONE_API_KEY
 )
 
-if "complaints" not in pc.list_indexes().names():
+if index_name not in pc.list_indexes().names():
     pc.create_index(
-        name="complaints",
+        name=index_name,
         dimension=2,
         metric="cosine",
         spec=ServerlessSpec(
@@ -24,3 +30,7 @@ if "complaints" not in pc.list_indexes().names():
             region='us-east-1'
         ) 
     )
+
+index = Pinecone.Index(index_name)
+
+client = OpenAI(api_key=OPENAI_API_KEY)
