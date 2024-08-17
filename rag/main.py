dc7c0eeb2da2cc51c261f0b3fba9819a4ce37571
harvey <harveytseng2@gmail.com>
2024-08-17 09:19:49 -0400

    creates script to insert vectorized sample data in pinecone

--- rag/main.py
+++ rag/main.py
@@ -1,6 +1,54 @@
 import asyncio
+import json
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+from pinecone import Pinecone
 
 from rag.utils.graph import build, stream
+
+
+def load_sample_data():
+    load_dotenv()
+
+    openai = OpenAI()
+    pinecone = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
+    pinecone_index = "complaints"
+    namespace = "rag_complaints"
+    index = pinecone.Index(pinecone_index)
+
+    model_name = "text-embedding-3-small"
+
+    data = json.loads(open("ruby_hackathon_data.json").read())
+
+    for complaint in data:
+        # Inserts the complaint into the Pinecone index with the complaint text as the vector
+        id = complaint['_id']
+        complaint_text = complaint['_source']['complaint_what_happened']
+        metadata = {
+            'product': complaint['_source']['product'],
+            'sub_product': complaint['_source']['sub_product']
+        }
+        # Create the embedding for the complaint text
+        raw_embedding = openai.embeddings.create(
+            input=[complaint_text],
+            model=model_name
+        )
+        embedding = raw_embedding.data[0].embedding
+
+        # Insert the complaint into the Pinecone index
+        index.upsert(
+            vectors=[
+                {
+                    "id": id,
+                    "values": embedding,
+                    "metadata": {"text": complaint_text, "product": metadata['product'],
+                                 "sub_product": metadata['sub_product']}
+                },
+            ],
+            namespace=namespace
+        )
 
 async def main():
     """
@@ -23,4 +71,5 @@ async def main():
     print(complaint)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    load_sample_data()
+    print("Sample data loaded")
