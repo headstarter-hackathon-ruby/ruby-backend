f16787004c3388dcefa693fa94b939eb0b128f2f
maishaSupritee <maisha.sup@gmail.com>
2024-08-16 23:38:37 -0700

    Add function to generate example data
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
 # main.py
 from fastapi import FastAPI
+import random
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
+def generate_example_data(numOfEntries):
+    data = []
+    for i in range(numOfEntries):
+        example_data = {
+            "id": i+1,
+            "product_category": "Credit card",
+            "sub_product_category": "General purpose credit cardor charge card",
+            "issue": "Sample issue",
+            "sub_issue": "Sample sub issue",
+            "complaint_what_happened": "Sample complaint",
+            "date_sent": (datetime.now() + timedelta(days=i)).isoformat()
+        }
+        data.append(example_data)
+    return data
+
+data = generate_example_data(5)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return data
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    for item in data:
+        if item["id"] == item_id:
+            return item
+        
+    return {"error": "Item not found"}
+    
 
 
 # uvicorn app:app --reload
