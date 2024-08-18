72572f19da1f30856f4471d17a61501f13011974
maishaSupritee <maisha.sup@gmail.com>
2024-08-18 10:24:11 -0700

    Fix typo
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- app.py
+++ app.py
@@ -580,7 +580,7 @@ async def add_transaction(transaction: TransactionCreate):
         raise HTTPException(status_code=500, detail=str(e))
     
 @app.post("/delete_transaction", description="Delete a transaction")
-async def add_transaction(transaction: TransactionCreate):
+async def delete_transaction(transaction: TransactionCreate):
     """
     This function removes a transaction from the user's account.
     """
